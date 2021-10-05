25d81d800c982aca6de5bfb40e890fe6b25dbb81
bogialmasi <boglarkaalmasi@gmail.com>
2021-10-05 14:57:57 +0200

    előkészítve JSON kommunikációra

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const http = require('http');
 const port = 4444;
 const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-    res.write(`A kérés forrása ${req.url}`);
-    res.end("       - Vége");
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    switch (true) {
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/index.html', (err, file) => {
+                res.setHeader('Content-Type', 'text/html');
+                res.end(file);
+            });
+            break;
+        case req.url === '/minta.css' && req.method === 'GET':
+            fs.readFile('./views/minta2.css', (err, file) => {
+                res.setHeader('Content-Type', 'text/css');
+                res.end(file);
+            });
+            break;
+        case req.url === '/telefon' && req.method === 'GET':
+            kiszolgal('./views/telefon.json', 'application/json', req, res);
+            break;
+        default:
+            kiszolgal('./views/hiba404.html', 'text/html', req, res);
+    };
 });
+
+function kiszolgal(filename, type, req, res) {
+    fs.readFile(filename, (err, file) => {
+        res.setHeader('Content-Type', type);
+        res.end(file);
+    });
+}
 server.listen(port);
